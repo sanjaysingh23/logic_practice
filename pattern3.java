5306c102487c6fe76ca745d937e52dc7968e5866
sanjay <sanjaychaudhary14121999@gmail.com>
2024-02-20 19:42:06 +0530

    first commit

---
+++ pattern3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        for(int i =1; i<=num; i++){
+            for(int k=num; k>=i;k--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
